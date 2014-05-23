76b824761ea15fac2e6100638cb1a7d29d5595d7
Luke <luke.monson@me.com>
2014-05-23 14:44:26 -0500

    first commit, getting the first 1000 prime numbers

---
+++ curtlukemonosn/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  curtlukemonosn
+//
+//  Created by lrm on 5/23/14.
+//  Copyright (c) 2014 Monson. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int lim;
+    int isPrime;
+    for(int x=2;x<1000;x++)
+    {
+        lim=(int) sqrt(x+1);
+        isPrime=1;
+        for(int y=2;y<=lim;y++){
+            if(x%y==0){
+                isPrime=0;
+                break; 
+            } 
+        } 
+        if(isPrime) 
+            cout << x << " "; 
+    } 
+    
+    return 0; 
+}
